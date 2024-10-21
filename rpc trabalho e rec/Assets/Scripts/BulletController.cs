0fecaefee3f86f0591137fb28f583f06af0f13b6
yasmin <yasmin.candiago@alunos.sc.senac.br>
2024-10-21 18:13:09 -0300

    OnTriggerEnter2D e mais um metodo

--- rpc trabalho e rec/Assets/Scripts/BulletController.cs
+++ rpc trabalho e rec/Assets/Scripts/BulletController.cs
@@ -46,4 +46,45 @@ public class BulletController : MonoBehaviourPun
             AutoDestruir();
         }
     }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Se a bala colidir com quem atirou, não faz nada
+        if (atirador == collision.gameObject)
+        {
+            Debug.Log("A bala passou pelo próprio atirador");
+            return;
+        }
+
+        // Vê se o objeto com quem a bala bateu pode levar dano
+        IDamageable damageable = collision.GetComponent<IDamageable>();
+        if (damageable != null)
+        {
+            // Aplica o dano ao objeto que pode receber dano
+            damageable.ReceberDano();
+
+            // Se a bala for "minha" (do jogador atual), adiciona um ponto ao acertar
+            if (photonView.IsMine)
+            {
+                // Adiciona ponto para o jogador
+                FindObjectOfType<PontuacaoManager>().AdicionarPontuacao(PhotonNetwork.LocalPlayer);
+            }
+        }
+        AutoDestruir();
+    }
+
+    void AutoDestruir()
+    {
+        // Apenas quem é dono da bala ou o host da partida pode destruir ela
+        if (photonView.IsMine)
+        {
+            // Destrói a bala
+            PhotonNetwork.Destroy(gameObject);
+        }
+        else if (PhotonNetwork.IsMasterClient)
+        {
+            // O host também pode destruir a bala
+            PhotonNetwork.Destroy(gameObject);
+        }
+    }
 }
