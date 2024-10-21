053a97e1c4108e0e8a99d7eee5542b46fa7da064
yasmin <yasmin.candiago@alunos.sc.senac.br>
2024-10-21 18:45:00 -0300

    resto do codigo

--- rpc trabalho e rec/Assets/Scripts/TankController.cs
+++ rpc trabalho e rec/Assets/Scripts/TankController.cs
@@ -33,5 +33,35 @@ public class TankController : MonoBehaviourPun, IDamageable
             MoverTanque(moverHorizonalmente, moverVerticalmente);
         }
     }
+
+    void MoverTanque(float moverHorizonalmente, float moverVerticalmente)
+    {
+        // movimento do tanque na direção que ele está apontando
+        Vector2 movimento = transform.right * moverVerticalmente * velocidadeMovimento * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + movimento);
+
+        // rotaciona o tanque (A ou D) - move no eixo Z para 2D
+        float rotacao = -moverHorizonalmente * velocidadeRotacao * Time.fixedDeltaTime;
+        rb.MoveRotation(rb.rotation + rotacao);
+    }
+
+    // método que trata o recebimento de dano
+    public void TakeDamage()
+    {
+        // quando o tanque leva dano, ele é teleportado para a área de respawn; por isso, envia uma mensagem ao dono do tanque para resetar a posição
+        photonView.RPC("ResetarPosicaoNoSpawn", photonView.Owner);
+    }
+
+    // método para resetar a posição do tanque
+    [PunRPC]
+    public void ResetarPosicaoNoSpawn()
+    {
+        // pega a posição do respawn com base no player
+        var localizacaoSpawn1 = FindFirstObjectByType<GameManager>().ObterLocalizacaoSpawn(photonView.Owner);
+
+        transform.position = localizacaoSpawn1.transform.position; // define a posição do tanque para o respawn
+
+        transform.rotation = localizacaoSpawn1.transform.rotation; // define a rotação do tanque para o respawn
+    }
 }
 
