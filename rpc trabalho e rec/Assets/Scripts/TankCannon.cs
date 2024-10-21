ea6f96cd48bd898de51c70debb2b734c93e43476
yasmin <yasmin.candiago@alunos.sc.senac.br>
2024-10-21 18:38:41 -0300

    variaveis

--- rpc trabalho e rec/Assets/Scripts/TankCannon.cs
+++ rpc trabalho e rec/Assets/Scripts/TankCannon.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 
 public class TankCannon : MonoBehaviourPun, IShootable
 {
-    
+    public Transform LocalizacaoSaidaBala;  // onde a bala vai sair do tanque
+
+    // tempo que o tanque leva para carregar e atirar
+    public float frequenciaBala = 1f;
+    public float frequenciaBalaAtual = 0f;
 }
 
