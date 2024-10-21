2f5fcb7f8ee26cfb5a008f0acf87ccb577c6dad6
yasmin <yasmin.candiago@alunos.sc.senac.br>
2024-10-21 18:29:18 -0300

    pontuação

--- rpc trabalho e rec/Assets/Scripts/ScoreManager.cs
+++ rpc trabalho e rec/Assets/Scripts/ScoreManager.cs
@@ -4,6 +4,19 @@ using Photon.Realtime;
 
 public class PontuacaoManager : MonoBehaviourPunCallbacks
 {
-   
+    public void AdicionarPontuacao(Player player) // adiciona 1 ponto ao jogador
+    {
+        int pontuacaoAtual = 0; // pega a pontuação atual do jogador, se existir
+        if (player.CustomProperties.ContainsKey("Pontuacao"))
+        {
+            pontuacaoAtual = (int)player.CustomProperties["Pontuacao"];
+        }
+        pontuacaoAtual += 1; // aumenta a pontuação em 1
+
+        // atualiza a pontuação no PhotonPun e avisa todo mundo, isso vai chamar o método OnPlayerPropertiesUpdate na classe PontuacaoUIController
+        Hashtable propriedadePontuacao = new Hashtable();
+        propriedadePontuacao["Pontuacao"] = pontuacaoAtual;
+        player.SetCustomProperties(propriedadePontuacao);
+    }
 }
 
