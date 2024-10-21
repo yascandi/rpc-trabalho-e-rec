0359eb172b4807ee4be219e32d73e63ffa84e0a4
yasmin <yasmin.candiago@alunos.sc.senac.br>
2024-10-21 20:36:01 -0300

    correção do codigo

--- rpc trabalho e rec/Assets/Scripts/BulletController.cs
+++ rpc trabalho e rec/Assets/Scripts/BulletController.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviourPun
 {
-    // Velocidade que a bala vai se mover
+    // velocidade que a bala vai se mover
     public float velocidade = 5f;
 
-    // Quanto tempo até a bala se destruir sozinha
+    // quanto tempo até a bala se destruir sozinha
     public float tempoDeVida = 3f;
     float tempoDeVidaAtual = 0f;
 
-    // Referência para o tanque que atirou
+    // referência para o tanque que atirou
     private GameObject atirador;
 
-    // Inicializa a bala e diz quem foi que atirou ela
+    // inicializa a bala e diz quem foi que atirou ela
     public void Inicializar(GameObject atirador)
     {
         this.atirador = atirador;
@@ -22,51 +22,35 @@ public class BulletController : MonoBehaviourPun
 
     void Update()
     {
-        // Faz a bala se mover para frente
-        MoveBala();
-
-        // Conta o tempo que a bala está "viva"
-        ContabilizarTempoDeVida();
-    }
-
-
-    void MoveBala()
-    {
-        // Move a bala
-        transform.Translate(Vector3.right * velocidade * Time.deltaTime);
-    }
-
-    void ContabilizarTempoDeVida()
-    {
-        // Atualiza o tempo de vida da bala
-        tempoDeVidaAtual += Time.deltaTime;
+        transform.Translate(Vector3.right * velocidade * Time.deltaTime); // movimenta a bala
+       
+        tempoDeVidaAtual += Time.deltaTime;  // atualiza o tempo de vida da bala
         if (tempoDeVidaAtual > tempoDeVida)
         {
-            // Quando o tempo acabar, destrói a bala
-            AutoDestruir();
+            AutoDestruir(); // quando o tempo acabar, destrói a bala
         }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        // Se a bala colidir com quem atirou, não faz nada
+        // se a bala colidir com quem atirou, não faz nada
         if (atirador == collision.gameObject)
         {
             Debug.Log("A bala passou pelo próprio atirador");
             return;
         }
 
-        // Vê se o objeto com quem a bala bateu pode levar dano
+        // vê se o objeto com quem a bala bateu pode levar dano
         IDamageable damageable = collision.GetComponent<IDamageable>();
         if (damageable != null)
         {
-            // Aplica o dano ao objeto que pode receber dano
+            // aplica o dano ao objeto que pode receber dano
             damageable.TakeDamage();
 
-            // Se a bala for "minha" (do jogador atual), adiciona um ponto ao acertar
+            // se a bala for "minha" (do jogador atual), adiciona um ponto ao acertar
             if (photonView.IsMine)
             {
-                // Adiciona ponto para o jogador
+                // adiciona ponto para o jogador
                 FindObjectOfType<ScoreManager>().AdicionarPontuacao(PhotonNetwork.LocalPlayer);
             }
         }
@@ -75,15 +59,15 @@ public class BulletController : MonoBehaviourPun
 
     void AutoDestruir()
     {
-        // Apenas quem é dono da bala ou o host da partida pode destruir ela
+        // apenas quem é dono da bala ou o host da partida pode destruir ela
         if (photonView.IsMine)
         {
-            // Destrói a bala
+            // destrói a bala
             PhotonNetwork.Destroy(gameObject);
         }
         else if (PhotonNetwork.IsMasterClient)
         {
-            // O host também pode destruir a bala
+            // o host também pode destruir a bala
             PhotonNetwork.Destroy(gameObject);
         }
     }
