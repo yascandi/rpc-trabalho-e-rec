992ccc6e5fd4275f769fbfc9edf5b9a5287e0d81
yasmin <yasmin.candiago@alunos.sc.senac.br>
2024-10-21 18:53:30 -0300

    iniciar a partida

--- rpc trabalho e rec/Assets/Scripts/GameManager.cs
+++ rpc trabalho e rec/Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviourPunCallbacks
@@ -22,7 +23,27 @@ public class GameManager : MonoBehaviourPunCallbacks
         textTimer.gameObject.SetActive(false);  // inicia como falso, pois o contador não deve aparecer antes de começar a partida
     }
 
-   
-   
+    public void IniciarPartida()  // método que inicia a partida
+    {
+        ehGameOver = false;
+        FindObjectOfType<ScoreManager>().ResetarPontuacao(PhotonNetwork.LocalPlayer);
+
+        tempoDePartidaAtual = tempoDePartida;  // faz o cronômetro aparecer
+
+        textTimer.gameObject.SetActive(true);
+        AtualizarTimerUI();
+
+        StartCoroutine(TimerCoroutine()); // inicia uma co-rotina que atualiza o tempo do cronômetro a cada 1 segundo
+
+        var go = ObterLocalizacaoSpawn(PhotonNetwork.LocalPlayer); // pega a localização do jogador para saber onde o tanque deve nascer
+
+        var tanque = PhotonNetwork.Instantiate("TanquePrefab", go.transform.position, go.transform.rotation); // cria o tanque no local certo
+    }
+
+    public GameObject ObterLocalizacaoSpawn(Player player)
+    {
+        var indice = (player.ActorNumber - 1) % localizacoesSpawn.Count;
+        return localizacoesSpawn[indice];
+    }
 }
 
